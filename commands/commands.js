34711beb8219bd1b8c866ccaabc831ebbeac06be
Andrés Villanueva <andresvillanuevab@gmail.com>
2018-02-09 11:59:03 -0300

    chore(App): add some enhancements

--- commands/commands.js
+++ commands/commands.js
@@ -1,7 +1,19 @@
 'use strict';
 
 const program = require('commander');
+const Table = require ('cli-table');
+
 const { getCommunities } = require('../src/service');
+
+const configTable = {
+    head: ['Nombre', 'Admins', 'Enlace'],
+    colWidths: [35, 50, 40],
+    style : {
+        compact : false,
+        'padding-left' : 1,
+        head: ['green'],
+    }
+};
 
 console.log('   ____                 _    ________');
 console.log('  / __ \\____  ___  ____| |  / / ____/');
@@ -10,25 +22,38 @@ console.log('/ /_/ / /_/ /  __/ / / / |/ / /___');
 console.log('\\____/ .___/\\___/_/ /_/|___/_____/');
 console.log('    /_/');
 
-
 program
     .version('0.1.0')
-    .usage('<comunidad>|<comunidades> [opciones]')
-    .description('Obten todas las comunidades de OpenVE en tu terminal');
+    .usage('<comunidad> | <comunidades> [opciones]')
+    .option('-l, --list', 'listado de todas las comunidades')
+    .description('Obten todas las comunidades de OpenVE en tu terminal 💻  🇻🇪');
 
-
-program
-    .command('comunidad')
-    .description('obten informacion sobre una comunidad')
-    .action( () => console.log('COMUNIDAD'));
 
 program
     .command('comunidades')
     .description('obten informacion sobre todas las comunidades')
     .option('-l, --list', 'listado de todas las comunidades')
-    .action( (arg) => {
-        return getCommunities()
-            .then(data => console.log('data: ', data.comunidades));
+    .action((cmd) => {
+        if (cmd.parent.rawArgs[3] === '-l' || cmd.parent.rawArgs[3] === '--list' ) {
+            console.log('\n ⏳ ...');
+            getCommunities()
+                .then((dataTable) => {
+                    let _table = new Table(configTable);
+                    dataTable.comunidades.map(item => {
+                        _table.push([item.name || '', item.admins || '', item.link || '']);
+                    });
+                    console.log(_table.toString());
+                });
+
+        } else {
+            program.help();
+        }
+
     });
 
+
 program.parse(process.argv);
+
+
+
+
